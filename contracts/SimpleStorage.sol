4f94a4042ef3e3eb768c1496e97927d04399ce2e
Deep Hansda <deephansda921@gmail.com>
2023-01-03 11:49:25 +0530

    first web 3.0 contract

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage{
+    string public name = "Deep Hansda";
+    int public year = 2023;
+    bool public isMarried = false;
+    address public add = 0x4fDbcD29A9EB9543A2bFA941e78B711C290BA2f5; 
+}
